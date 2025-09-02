e18e8e9a23d29bd7b4af8b388ac3a9c06fc96803
Amir Daoudi <amir@daoudi.org>
2025-09-01 20:14:45 -0400

    Remove secrets and add proper gitignore

--- backend/dist/controllers/TemperatureController.d.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-export declare class TemperatureController {
-    static getCurrentTemperature(req: Request, res: Response): Promise<void>;
-    static submitVote(req: Request, res: Response): Promise<void>;
-    static getTemperatureStats(req: Request, res: Response): Promise<void>;
-    static getZones(req: Request, res: Response): Promise<void>;
-}
-export default TemperatureController;
-//# sourceMappingURL=TemperatureController.d.ts.map
